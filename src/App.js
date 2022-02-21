af6ea39d6743a14d81b11cf14dc05b391ab948f5
Koichi <koichi.p.noda@gmail.com>
2022-02-21 20:05:50 +0900

    feat: add smart contract call func with web3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { ethers } from "ethers"
+import simpleContractAbi from "./abi/SimpleContract.json"
+import './App.css'
 
-function App() {
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
+const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+// get the end user
+const signer = provider.getSigner()
+
+// get the smart contract
+const contract = new ethers.Contract(contractAddress, simpleContractAbi, signer)
+
+const App = () => {
+  const [name, setName] = useState('')
+  const [currentAccount, setCurrentAccount] = useState(null)
+
+  const checkWalletConnected = () => {
+    const { ethereum } = window
+    
+    if (!ethereum) {
+      console.log("Make sure that you have MetaMask installed")
+      return
+    } else {
+      console.log("Wallet exists! You are ready to go")
+    }
+  }
+
+  const handleConnect = async () => {
+    const { ethereum } = window
+
+    if (!ethereum) {
+      alert("Please install metamask")
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
+      setCurrentAccount(accounts[0])
+
+    } catch (e) {
+      console.log("Error: ", e)
+    }
+
+  }
+
+  const handleGet = async () => {
+    const greet = await contract.greet()
+    console.log("Greet:  ", greet)
+  }
+
+  const handleSubmit = async () => {
+    const res = await contract.setGreeting(name)
+    console.log("Res: ", res)
+  }
+
+  useEffect(() => {
+    checkWalletConnected()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGet}>Get</button>
+      <div>
+        <input value={name} onChange={e => setName(e.target.value)}></input>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <button onClick={handleConnect}>Connect</button>
     </div>
   );
 }
 
-export default App;
+export default App
